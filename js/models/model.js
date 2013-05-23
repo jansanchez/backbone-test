8f4dce9875d70bba686c0b3131298aa2782850a5
Jan Sanchez <joejansanchez@gmail.com>
2013-05-23 14:54:03 -0500

    bb-test v0.0.1rc9

--- js/models/model.js
+++ js/models/model.js
@@ -1,6 +1,8 @@
 
+/*cambio el template settings de underscore para usar {{dato}} en mis plantillas*/
 _.templateSettings = { interpolate : /\{\{(.+?)\}\}/g };
 
+/*Creo un modelo*/
 var Imagen = Backbone.Model.extend({
 	defaults : {
 		src : 'none.jpg',
@@ -12,24 +14,18 @@ var Imagen = Backbone.Model.extend({
 		this.on('change:title', function(){
 			console.log('se cambio el title a: '+this.get('title'));
 		});
-		this.on('change:src', function(){
-			console.log('se cambio el src a: '+this.get('src'));
-		});
-		this.on('change:main', function(){
-			console.log('se cambio el main a: '+this.get('main'));
-		});
         this.on('change',function(){
-            console.log(this.cid);
+            this.view.render();
         });
 	},
 	setSrc : function(src){
-		this.set({'src':src});
+		this.set({'src' : src});
 	},
 	setTitle : function(title){
-		this.set({'title':title});
+		this.set({'title' : title});
 	},
 	setMain : function(main){
-		this.set({'main':main});
+		this.set({'main' : main});
 	}
 });
 
@@ -37,11 +33,10 @@ var Imagenes = Backbone.Collection.extend({
 	model : Imagen
 });
 
-var imgs = new Imagenes();
-
-var ImagenView = Backbone.View.extend({
+var GalleryView = Backbone.View.extend({
 	el : $('#divGallery'),
 	contador : 0,
+	model: null,
 	events: {
 		"click #btnChoose" : "addImg"
 	},
@@ -51,18 +46,14 @@ var ImagenView = Backbone.View.extend({
 		this.model.on("remove", this.removeOne);
 	},
 	render : function(){
-		/*
-		var template = $.trim($('.thumb').html());
-		this.$el.append(template);
-		*/
-		return this;
+		/*aqui renderizo la vista principal, la cargo con datos si deseo*/
 	},
 	addImg: function(){
 		this.contador++;
-		imgs.add({src: this.contador+'.jpg', title: this.contador});
+		this.model.add({src: this.contador+'.jpg', title: this.contador});
 	},
 	addOne : function(modelo){
-		modelo.view = new iView({model : modelo});
+		modelo.view = new galleryRow({model : modelo});
 		this.$('.dragger').append(modelo.view.render().el);
 	},
 	removeOne : function(modelo){
@@ -71,26 +62,36 @@ var ImagenView = Backbone.View.extend({
 	}
 });
 
-var mainView = new ImagenView({
-	model : imgs
+var mainView = new GalleryView({
+	model : (function(){
+		return new Imagenes();
+	})()
 });
 
-var iView = Backbone.View.extend({
+var galleryRow = Backbone.View.extend({
 	className : 'thumb',
 	template : null,
 	events: {
-        "click .remove" : "deleteImage"
+        "click .remove" : "deleteImage",
+        "click .main" : "aaaBbb"
 	},
-	initialize: function() {
+	initialize: function(){
 		_.bindAll(this);
 		this.template = $.trim($('.tpl .thumb').html());
 	},
 	render : function(){
-		var compiled_template = _.template( this.template );
-		this.$el.html( compiled_template(this.model.toJSON()));
+		var compiled_template = _.template(this.template);
+		this.$el.html(compiled_template(this.model.toJSON()));
 		return this;
 	},
     deleteImage: function(){
-		this.model.collection.remove(this.model);
+    	this.model.collection.remove(this.model);
+    },
+    aaaBbb: function(){
+    	if (this.model.get('main')==1) {
+    		this.model.setMain(0);
+    	}else{
+    		this.model.setMain(1);
+    	}
     }
 });
